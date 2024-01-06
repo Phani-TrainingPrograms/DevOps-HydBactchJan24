6a9f1b99fd5647e8be6064f1f19d570ec2d66455
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2024-01-06 14:42:23 +0530

    Docker code example
    Create the docker instructions as mentioned in the docker file

---
+++ JavaApp/SampleFile.java
@@ -0,0 +1,6 @@
+public class SampleFile{
+    public static void main(String[] args) {
+        System.out.println("Sample Example of java App");
+        System.out.println("Sample Example of java App");
+    }
+}
